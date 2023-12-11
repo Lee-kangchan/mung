c309bb8a15af72fb57d296d0bbc7175efec1e451
lee-kangchan <lsc4237@gmail.com>
2023-12-11 17:45:35 +0900

    Gradle 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,23 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-rest")
+    implementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "junit")
+        exclude(module = "spring-test")
+    }
+    implementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    implementation("mysql:mysql-connector-java") // MySQL
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    testImplementation("io.kotest:kotest-core:4.6.0") // Kotest core 모듈
+    testImplementation("io.kotest:kotest-assertions-core:4.6.0") // Kotest assertions 모듈
+    testImplementation("io.kotest:kotest-runner-junit5:4.6.0") // Kotest JUnit5 지원 모듈
+    testImplementation("io.kotest:kotest-extensions-spring:4.6.0") // Kotest Spring 지원 모듈
+    // 추가로 필요한 Kotest 모듈을 여기에 추가할 수 있습니다.
 }
 
 tasks.withType<KotlinCompile> {
